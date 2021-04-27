57746abc8f1b70350a892a67b5c8645e4a2770c7
Stefan Ravninov <SORavninov19@codingburgas.bg>
2021-04-27 21:38:23 +0300

    add log-in

---
+++ Website/assest/js/names.js
@@ -0,0 +1,347 @@
+const names = [
+
+    {
+        name: "Mason Smith",
+        age: 26,
+    },
+
+    {
+        name: "Ethan Smith",
+        age: 36,
+    },
+
+    {
+        name: "Noah Williams",
+        age: 32,
+    },
+    {
+        name: "William Thomas",
+        age: 19,
+    },
+    {
+        name: "Liam Medina",
+        age: 24,
+    },
+    {
+        name: "Michael Hicks",
+        age: 43,
+    },
+    {
+        name: "Jayden Williams",
+        age: 26,
+    },
+    {
+        name:
+            "Alexander Brown",
+        age: 40,
+    },
+    {
+        name: "Aiden Miller",
+        age: 28,
+    },
+    {
+        name: "Daniel Medina",
+        age: 21,
+    },
+    {
+        name: "Matthew Medina",
+        age: 41,
+    },
+    {
+        name: "Elijah Sweeney",
+        age: 38,
+    },
+    {
+        name: "Sophia Miller",
+        age: 25,
+    },
+    {
+        name: "Emma Knight",
+        age: 27,
+    },
+    {
+        name: "Isabella Chapman",
+        age: 34,
+    },
+    {
+        name:
+            "Olivia Knight",
+        age: 37,
+    },
+    {
+        name: "Ava Chapman",
+        age: 26,
+    },
+    {
+        name: "Emily Franklin",
+        age: 36,
+    },
+    {
+        name: "Abigail Knight",
+        age: 28,
+    },
+    {
+        name: "Mia Williams",
+        age: 30,
+    },
+    {
+        name: "Madison Franklin",
+        age: 46,
+    },
+    {
+        name: "Elizabeth Daniel",
+        age: 20,
+    },
+    {
+        name: "Chloe Knight",
+        age: 46,
+    },
+    {
+        name:
+            "Ella Bowman",
+        age: 41,
+    },
+    {
+        name: "Avery Hale",
+        age: 40,
+    },
+    {
+        name: "Addison Dixon",
+        age: 54,
+    },
+    {
+        name: "Aubrey Richards",
+        age: 24,
+    },
+    {
+        name: "Jacob Watkins",
+        age: 19,
+    },
+    {
+        name: "Mason Brown",
+        age: 56,
+    },
+    {
+        name: "Ethan Mejia",
+        age: 46,
+    },
+    {
+        name: "Noah Walters",
+        age: 46,
+    },
+    {
+        name: "William Becker",
+        age: 46,
+    },
+    {
+        name:
+            "Liam Miller",
+        age: 46,
+    },
+    {
+        name: "Michael Barnes",
+        age: 46,
+    },
+    {
+        name: "Jayden Brown",
+        age: 26,
+    },
+    {
+        name: "Alexander Keith",
+        age: 42,
+    },
+    {
+        name: "Aiden Holt",
+        age: 32,
+    },
+    {
+        name: "Daniel Miller",
+        age: 23,
+    },
+    {
+        name: "Matthew Miller",
+        age: 36,
+    },
+    {
+        name: "Elijah Mcdonald",
+        age: 23,
+    },
+    {
+        name: "Sophia Thomas",
+        age: 42,
+    },
+    {
+        name: "Emma Williams",
+        age: 36,
+    },
+    {
+        name: "Isabella Wilcox",
+        age: 46,
+    },
+    {
+        name: "Olivia Holt",
+        age: 24,
+    },
+    {
+        name: "Ava Knight",
+        age: 41,
+    },
+    {
+        name: "Emily Simmons",
+        age: 52,
+    },
+    {
+        name: "Abigail Gilbert",
+        age: 32,
+    },
+    {
+        name: "Mia Wade",
+        age: 45,
+    },
+    {
+        name:
+            "Madison Beck ",
+        age: 32,
+    },
+    {
+        name: "Elizabeth Wells",
+        age: 42,
+    },
+    {
+        name: "Chloe Smith",
+        age: 23,
+    },
+    {
+        name: "Ella Newman",
+        age: 45,
+    },
+    {
+        name: "Avery Beck",
+        age: 36,
+    },
+    {
+        name: "Addison Willis",
+        age: 26,
+    },
+    {
+        name: "Aubrey Willis",
+        age: 36,
+    },
+    {
+        name: "Jacob Wu",
+        age: 42,
+    },
+    {
+        name:
+            "Mason Simmons",
+        age: 32,
+    },
+    {
+        name: "Ethan Navarro",
+        age: 20,
+    },
+    {
+        name: "Noah Simmons",
+        age: 34,
+    },
+    {
+        name: "William Chandler",
+        age: 25,
+    },
+    {
+        name: "Liam Knight",
+        age: 19,
+    },
+    {
+        name: "Michael Chandler",
+        age: 27,
+    },
+    {
+        name: "Jayden Williams",
+        age: 24,
+    },
+    {
+        name:
+            "Alexander Snyder",
+        age: 23,
+    },
+    {
+        name: "Aiden Park",
+        age: 21,
+    },
+    {
+        name: "Daniel Chandler",
+        age: 25,
+    },
+    {
+        name: "Matthew Snyder",
+        age: 27,
+    },
+    {
+        name: "Elijah Snyder",
+        age: 28,
+    },
+    {
+        name: "Sophia Park",
+        age: 29,
+    },
+    {
+        name: "Emma Brown",
+        age: 30,
+    },
+    {
+        name: "Isabella Thomas",
+        age: 31,
+    },
+    {
+        name:
+            "Olivia Thomas",
+        age: 53,
+    },
+    {
+        name: "Ava Brown",
+        age: 46,
+    },
+    {
+        name: "Emily Bowman",
+        age: 46,
+    },
+    {
+        name: "Abigail Mason",
+        age: 36,
+    },
+    {
+        name:
+            "Mia Mason",
+        age: 47,
+    },
+    {
+        name: "Madison Thomas",
+        age: 14
+        ,
+    },
+    {
+        name: "Elizabeth Brady",
+        age: 52,
+    },
+    {
+        name: "Chloe Brown",
+        age: 21,
+    },
+    {
+        name: "Ella Thomas",
+        age: 25,
+    },
+    {
+        name: "Avery Brady",
+        age: 36,
+    },
+    {
+        name: "Addison Gillespie",
+        age: 50,
+    },
+    {
+        name: "Aubrey Knight",
+        age: 34,
+
+    }]
---
+++ Website/assest/js/script.js
@@ -0,0 +1,67 @@
+
+function searchNames() {
+    document.getElementById("results").innerHTML = ""
+    searchBar = document.getElementById("search").value.toLowerCase();
+    let possibleResults = names.filter(elem => elem.toLowerCase().startsWith(searchBar));
+
+    for (let result of possibleResults) {
+        document.getElementById("results").innerHTML += "<li>" + result + "</li>";
+    }
+}
+
+function report() {
+    var x = document.getElementById("x").value;
+    var y = document.getElementById("y").value;
+    console.log(x, y);
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoidG9ybmFkbzgzMSIsImEiOiJja255czhpNnoxamo5Mm5vYXNqazdlOWRvIn0.WoT_wLRU5OcwNVLpxe1BKA';
+
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [x, y],
+        zoom: 3
+    });
+
+    var geojson = {
+        features: [{
+            geometry: {
+                type: 'Point',
+                coordinates: [x, y]
+            },
+
+        },]
+    };
+    geojson.features.forEach(function (marker) {
+
+        var el = document.createElement('div');
+        el.className = 'marker';
+
+        new mapboxgl.Marker(el)
+            .setLngLat(marker.geometry.coordinates)
+            .addTo(map);
+    });
+}
+function submitAcc() {
+    let adminAccs = [
+        {
+            name: "admin",
+            pass: "admin123",
+        },
+        {
+            name: "admin2",
+            pass: "admin1234",
+        }]
+    let employeesAccs = [{
+        name: "Ivan",
+        pass: "password",
+    }]
+    let name = document.getElementById("name").value;
+    let pass = document.getElementById("pass").value;
+    if (name = adminAccs[0].name && pass == adminAccs[0].pass) {
+        document.getElementById("nav").style = "display: block"
+    }
+    if (name = adminAccs[0].name && pass == adminAccs[0].pass) {
+        document.getElementById("nav").style = "display: block"
+    }
+}
